d676325affcbb0e5d421834646858a15c82255b9
ngoducphi12761 <33415429+ngoducphi12761@users.noreply.github.com>
2024-10-28 12:28:32 -0500

    Update Main.java add a test

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
             public static void main(){
             System.out.println("Hello world");
             System.out.println("Hello Fairfield");
+		    // add 1
 	    }
 }
